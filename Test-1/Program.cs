8c04cd5c5d3cccf7e95f682431a84171c08b2214
ahka07001 <ahka07001@E-PW05CGQT>
2024-09-12 13:49:22 +0200

    Add project files.

---
+++ Test-1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Uppgift2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            //Detta är kommentar test
+            Console.WriteLine("Detta är ett program gjort i C#\");
+            Console.WriteLine("Programmet skriver ut text på flera rader");
+            Console.WriteLine("Tryck på valfri tangent för att avsluta programmet");
+        }
+    }
+}
